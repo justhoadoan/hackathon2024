982a060fcafb4e6c6d27221851c61e21eb66e120
justhoadoan <hoatrongdoan1710@gmail.com>
2024-10-20 10:10:17 +0700

    update

--- preprocessingdata.py
+++ preprocessingdata.py
@@ -1,6 +1,13 @@
 import os
 import shutil
+import pandas as pd
 
+def modify_txt_file(file_path):
+    
+    df = pd.read_csv(file_path, header=None, delim_whitespace=True)
+    df.iloc[:, 0] = df.iloc[:, 0].apply(lambda x: x - 4 if x >= 4 else x)
+    df.to_csv(file_path, header=False, index=False, sep=' ')
+        
 files_train = os.listdir('./train/')
 file_test= os.listdir('./public test/')
 train_path_img = "./yolo_data/images/train/"
@@ -16,19 +23,22 @@ os.makedirs(val_path_img, exist_ok = True)
 os.makedirs(val_path_label, exist_ok = True)
 
 path= "/train/"
-os.makedirs(val_path_img, exist_ok = True)
-os.makedirs(val_path_label, exist_ok = True)
-for filex in files_train[:int(len(files_train)*split)]:
+
+for filex in files_train[:int(len(files_train)*(1-split))]:
+      
       file_path=os.path.join('./train/', filex)
       
       if filex.endswith(".txt"):
+         
+          modify_txt_file(file_path)        
           shutil.copy(file_path, train_path_label)
       else:
-            shutil.copy(file_path, train_path_img)
-for filex in files_train[int(len(files_train)*split):]:
+          shutil.copy(file_path, train_path_img)
+for filex in files_train[int(len(files_train)*(1-split)):]:
       file_path=os.path.join('./train/', filex)
       
       if filex.endswith(".txt"):
+          modify_txt_file(file_path)
           shutil.copy(file_path, val_path_label)
       else:
             shutil.copy(file_path, val_path_img)
